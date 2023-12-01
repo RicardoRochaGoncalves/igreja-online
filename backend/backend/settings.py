e19a21f92989ede9ee508d88794ffa35cbb139ae
Ricardo Rocha Gonçalves <ricardo.rg.neto@gmail.com>
2023-12-01 13:57:47 -0300

    Alteração allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'www.activesolucoes.net.br', 'activesolucoes.net.br', '154.56.43.58']
+
 
 
 # Application definition
